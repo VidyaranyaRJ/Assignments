84c2b745a106e97f541522e33f67016f1627fe9a
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-01-18 14:04:44 -0700

    first commit

---
+++ Python Assignments/Beginner/Q1.py
@@ -0,0 +1,9 @@
+for num in range(1, 16):
+    if num % 3 == 0 and num % 5 == 0:
+        print("Consultadd - Python Training")
+    elif num % 3 == 0:
+        print("Consultadd")
+    elif num % 5 == 0:
+        print("Python Training")
+    else:
+        print(num)
