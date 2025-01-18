07a0fd52da1338ced2ba363debe2212a8a4bae86
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-01-18 14:56:48 -0700

    Adv q7

---
+++ Python Assignments/Advance/Q7.py
@@ -0,0 +1,5 @@
+students = ['Sam', 'Alice', 'Mona']
+subjects = ['Commerce', 'Science', 'Computer']
+
+student_subject_dict = {students[i]: subjects[i] for i in range(len(students))}
+print(student_subject_dict)
